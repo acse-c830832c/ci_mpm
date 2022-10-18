d9c50406cce240ba4fddabc1d169ae5477aa32d1
acse-c830832c <xtqyzli@gmail.com>
2022-10-18 12:11:28 +0100

    add factorial function_2

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -13,12 +13,3 @@ def my_sum(iterable):
 def factorial(n):
     return n * factorial(n-1) if n else 1
 
-    @pytest.mark.parametrize('number, expected', [
-        (5, 120),
-        (3, 6),
-        (1, 1)
-    ])
-    def test_factorial(self, number, expected):
-        '''Test our factorial function'''
-        answer = factorial(number)
-        assert answer == expected
